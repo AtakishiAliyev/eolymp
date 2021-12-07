f4b6b2438c437dccd92d30bab5b31a0e371d8464
atakishialiyev <atakishieliyev@gmail.com>
2021-12-08 00:01:13 +0400

    New tasks done

--- Loops/8255.js
+++ Loops/8255.js
@@ -1,1 +1,16 @@
 // https://www.eolymp.com/az/problems/8255
+let n = 3;
+let s = 25;
+
+for(let i = 10 ** (n - 1); i < 10 ** n; i++) {
+    a = i;
+    let sum = 0;
+    while(a > 0) {
+        b = a % 10;
+        sum += b;
+        a = parseInt(a / 10);
+    }
+    if(sum === s) {
+        console.log(i);
+    }
+}
---
+++ Loops/8533.js
@@ -0,0 +1,4 @@
+// https://www.eolymp.com/az/problems/8533
+
+// let a = 2011;
+// let b = 2013;
