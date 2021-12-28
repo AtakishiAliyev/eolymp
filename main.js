13047ae9e1a6d06b167193d07adc709c6634d603
Atakishi <atakishieliyev@gmail.com>
2021-12-29 00:09:50 +0400

    Done

--- main.js
+++ main.js
@@ -1,72 +1,4 @@
-// Task 1
-// let n = 3;
-// let star = ""
-// let res = "";
-
-// for(let i = 1; i <= n; i++) {
-//     star += "*"
-// }
-
-// for(let j = 1; j <= n; j++) {
-//     res = res + "\n" + star
-// }
-
-// console.log(res);
-
-// Task 2
-// let n = 5;
-// let res = "";
-
-// for(let i = 1; i <= n; i++) {
-//     res += "*"
-//     console.log(res);
-// }
-
-
-// Task 3
-// let n = 3;
-// let arr = [];
-
-// for(let i = 0; i < n; i++){
-//     arr.push(new Array(n));
-
-//     for(let j = 0; j < n; j++){
-//         if(i + j + 1 == n) {
-//             arr[i][j] = 0;
-//         }
-//         else if(i == 0 || j == 0) {
-//             arr[i][j] = 2;
-//         }
-//         else {
-//             arr[i][j] = 1;
-//         }
-//     }
-// }
-// console.log(arr);
-
-
-// Task 4
-// let n = 5;
-// let m = 2;
-// let x = 4;
-// let y = 2;
-// let matrix = [];
-// let arr = [];
-
-// for(let i = 0; i < n * m; i++) {
-//     if(i % 2 === 0) {
-//         arr = [];
-//         arr.push(i)
-//     } else {
-//         arr.push(i)
-//         matrix.push(arr)
-//     }
-// }
-
-// console.log(matrix[x - 1][y - 1]);
-
-
-// Task 5
+//Task
 let n = 5;
 let i = 4;
 let j = 2;
@@ -93,7 +25,7 @@ while (startCol <= endCol && startRow <= endRow) {
         res[i][endCol] = count; 
         count++;
     }
-    endCol--; 
+    endCol--;   
 
     for (let i = endCol; i >= startCol; i--) {
         res[endRow][i] = count;
@@ -107,5 +39,4 @@ while (startCol <= endCol && startRow <= endRow) {
     }
     startCol++;
 }
-
 console.log(res[i - 1][j - 1]);
